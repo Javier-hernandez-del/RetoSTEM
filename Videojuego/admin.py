730ce4d7f260f26af07001c1fa44cb00982b50de
DiegoManzanarez <A01653042@itesm.mx>
2021-04-04 21:58:24 -0500

    #3 ModelsAct

--- Videojuego/admin.py
+++ Videojuego/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from . models import MinutosJugados
+from . models import Usuarios
 from . models import Edades
 
 # Register your models here.
-admin.site.register(MinutosJugados)
+admin.site.register(Usuarios)
 admin.site.register(Edades)
