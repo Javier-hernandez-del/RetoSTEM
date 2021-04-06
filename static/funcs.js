553d124f54f6b85a2dc5bfc26810fcc3d76ab47c
Javier-hernandez-del <57495064+Javier-hernandez-del@users.noreply.github.com>
2021-04-06 18:22:56 -0500

    Update funcs.js

--- static/funcs.js
+++ static/funcs.js
@@ -27,7 +27,7 @@ function myFunction3() {
 
 
 function showPicture() {
-  var sourceOfPicture = "RetoSTEM/static/funcs.js/IM1.png";
+  var sourceOfPicture = "RetoSTEM/static/IM1.png";
   var img = document.getElementById('image')
   img.src = sourceOfPicture.replace('30x30', '30x30');
   img.style.display = "block";
