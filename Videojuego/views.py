307bd0da6380d09d362d909a3b2b4d81fe6bb7db
DiegoManzanarez <73494766+DiegoManzanarez@users.noreply.github.com>
2021-04-12 16:39:34 -0500

    Update views.py

--- Videojuego/views.py
+++ Videojuego/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from json import loads
 from . models import Usuarios
 from . models import Edades
 
