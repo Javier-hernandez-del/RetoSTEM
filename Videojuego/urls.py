58934c66f11786d2e0a6e86cc8ef47a581b3fd8c
DiegoManzanarez <A01653042@itesm.mx>
2021-04-12 15:28:56 -0500

    Update urls.py

--- Videojuego/urls.py
+++ Videojuego/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('GAMESTEAM',views.GAMESTEAM, name = 'GAMESTEAM'),
     path('Estadisticas',views.Estadisticas, name = 'Estadisticas'),
     path('datos',views.datos, name = 'datos'),
+    path('unity',views.unity, name = 'unity'),
     #path('datos2',views.datos2, name = 'datos2'),
 ]
