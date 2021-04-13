66b8d0836faf8af44e1f7fa678dcccd387780e77
DiegoManzanarez <A01653042@itesm.mx>
2021-04-13 17:52:12 -0500

    FinalUpdate

--- RetoSTEM/settings.py
+++ RetoSTEM/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'RetoSTEM.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'steamdb',
@@ -84,6 +84,13 @@ DATABASES = {
         'PASSWORD': 'STEAMDBpsw07',
         'HOST': 'localhost',
         'PORT': '5432',
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
